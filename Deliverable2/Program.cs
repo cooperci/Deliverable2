6cd100ec47e96af357758da0d158593e6f8ad933
cooperci <ceci.cooper.4@gmail.com>
2020-09-24 23:05:20 -0400

    deliverable2

--- Deliverable2/Program.cs
+++ Deliverable2/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-
+using System.Globalization;
 namespace Deliverable2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Guess which will have more: heads or tails? ");
+            string headsOrTailsGuess = Console.ReadLine().ToLower();
+            Console.Write("How many times shall we flip a coin? ");
+            int numberOfFlips = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            int count = 0;
+            decimal correctCount = 0;
+            while (count < numberOfFlips)
+            {
+                Random rnd = new Random();
+                int result = rnd.Next(0, 2);
+                if (result == 1)
+                {
+                    if (headsOrTailsGuess == "tails")
+                    {
+                        correctCount += 1;
+                    }
+                    Console.WriteLine("tails");
+                    count += 1;
+                }
+                else if (result == 0)
+                {
+                    if (headsOrTailsGuess == "heads")
+                    {
+                        correctCount += 1;
+                    }
+                    Console.WriteLine("heads");
+                    count += 1;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Your guess, {headsOrTailsGuess}, came up {correctCount} times (s).");
+            decimal coinTossPercentage = correctCount / numberOfFlips;
+            Console.WriteLine($"That's {coinTossPercentage.ToString("P", CultureInfo.InvariantCulture)}");
         }
     }
 }
